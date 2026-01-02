dc4f92a705a646709d7ed98bd340016bca932038
Leo <leonidas01032002@gmail.com>
2026-01-02 16:19:38 +0100

    löscht bei deny changes

--- Assets/RuntimeChangesSaver/Editor/ChangesTracker.cs
+++ Assets/RuntimeChangesSaver/Editor/ChangesTracker.cs
@@ -1082,10 +1082,48 @@ namespace RuntimeChangesSaver.Editor
     }
     else
     {
-        Debug.Log($"[PlayOverrides][ProcessNextSceneInQueue] User chose DISCARD for '{currentPath}'. Scheduling next scene...");
+        Debug.Log($"[PlayOverrides][ProcessNextSceneInQueue] User chose DISCARD for '{currentPath}'. Removing changes from store for this scene...");
+        RemoveChangesForSceneFromStore(currentPath, tStore, cStore);
         EditorApplication.delayCall += () => ProcessNextSceneInQueue(remainingScenes, startScenePath, tStore, cStore);
     }
 }
+
+        private static void RemoveChangesForSceneFromStore(string targetScenePath, TransformChangesStore tStore, ComponentChangesStore cStore)
+        {
+            targetScenePath = NormalizeScenePath(targetScenePath);
+            
+            Debug.Log($"[PlayOverrides][RemoveChangesForSceneFromStore] Removing changes for scene '{targetScenePath}'");
+
+            if (tStore != null && tStore.changes.Count > 0)
+            {
+                var transformChangesToRemove = tStore.changes.FindAll(c => 
+                    string.Equals(NormalizeScenePath(c.scenePath), targetScenePath, StringComparison.OrdinalIgnoreCase));
+                
+                foreach (var change in transformChangesToRemove)
+                {
+                    tStore.changes.Remove(change);
+                    Debug.Log($"[PlayOverrides][RemoveChangesForSceneFromStore] Removed Transform change for '{change.objectPath}'");
+                }
+            }
+
+            if (cStore != null && cStore.changes.Count > 0)
+            {
+                var componentChangesToRemove = cStore.changes.FindAll(c => 
+                    string.Equals(NormalizeScenePath(c.scenePath), targetScenePath, StringComparison.OrdinalIgnoreCase));
+                
+                foreach (var change in componentChangesToRemove)
+                {
+                    cStore.changes.Remove(change);
+                    Debug.Log($"[PlayOverrides][RemoveChangesForSceneFromStore] Removed Component change for '{change.objectPath}' (type: {change.componentType})");
+                }
+            }
+
+            EditorUtility.SetDirty(tStore);
+            EditorUtility.SetDirty(cStore);
+            AssetDatabase.SaveAssets();
+            
+            Debug.Log($"[PlayOverrides][RemoveChangesForSceneFromStore] Changes for scene '{targetScenePath}' have been removed and saved");
+        }
 
         // Auch CheckReturnToStartScene mit mehr Delays:
         private static void CheckReturnToStartScene(string startPath)
